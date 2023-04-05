25095b3b58ae4618aae8a06831513c3433f61571
TWallace <tomwallace75@hotmailcom>
2023-04-04 19:11:39 -0700

    Removed jquery

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-$(document).ready(() => {
-  const $indy = $('#indy')
-  const $maleSelect = $('#male-dogs')
-  const $femaleSelect = $('#female-dogs')
+// $(document).ready(() => {
+//   const $indy = $('#indy')
+//   const $maleSelect = $('#male-dogs')
+//   const $femaleSelect = $('#female-dogs')
 
-  $maleSelect.change(() => {
-    $('.dogInfo').removeClass('active')
-    const selected = $maleSelect.find("option:selected").val()
-    const $indy = $('#indy')
-    switch (selected) {
-      case 'indy':
-        $indy.addClass('active')
-        break
-    }
-  })  
-})
+//   $maleSelect.change(() => {
+//     $('.dogInfo').removeClass('active')
+//     const selected = $maleSelect.find("option:selected").val()
+//     const $indy = $('#indy')
+//     switch (selected) {
+//       case 'indy':
+//         $indy.addClass('active')
+//         break
+//     }
+//   })  
+// })
